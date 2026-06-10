5e372c852966196ae99bf311e0577849808262a8
UpSkillPro <upskillpro@users.noreply.github.com>
2026-06-10 21:43:57 +0100

    Connect ESOL assessment submissions

--- src/esolAssessmentConfig.js
+++ src/esolAssessmentConfig.js
@@ -1,5 +1,5 @@
 export const ESOL_ASSESSMENT_CONFIG = {
-  googleAppsScriptUrl: '',
+  googleAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbyHAm-MZ3SkNKHAbiIrhSwtAFsBe50OaavFCno8ayj8LaCH852-yTzNMW6KxXeREMAj/exec',
   localStorageKey: 'upskillpro-esol-initial-assessment-submitted',
   duplicateWindowHours: 24,
 };
